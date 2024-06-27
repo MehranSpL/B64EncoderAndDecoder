e5181cea17038bba712d92e7bf25e1b68d26d372
MehranSpL <126176342+MehranSpL@users.noreply.github.com>
2024-06-28 00:24:28 +0330

    Update main.py

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ from pystyle import *
 print (Fore.RED + '1 - B64 Encoder')
 print (Fore.BLUE + '2 - B64 Decoder')
 
-b = Write.Input("Kodom File Ro Load Bedam? -> ", Colors.red_to_purple, interval=0.0025)
+b = Write.Input("Your selection? -> ", Colors.red_to_purple, interval=0.0025)
 Write.Print(f"Successfully Selected {b} !", Colors.green_to_yellow, interval=0.05)
 
 if b=='1' :
-    print(' | Sabr Daram Load Midam...')
+    print(' | Wait, we're loading Encoder...')
     time.sleep(1)
     os.system('cmd /k "python scripts/encoder.py"')
 
 if b=='2' :
-    print(' | Sabr Daram Load Midam...')
+    print(' | Wait, we're loading Decoder...')
     time.sleep(1)
     os.system('cmd /k "python scripts/decoder.py"')
